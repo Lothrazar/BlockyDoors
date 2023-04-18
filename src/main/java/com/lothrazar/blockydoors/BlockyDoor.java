d24e1a905d404d00fe8c8fa990c3a1bc28498e86
lothrazar <samson.bassett@gmail.com>
2023-04-18 12:24:15 -0700

    1.19.4 ported andesite

--- src/main/java/com/lothrazar/blockydoors/BlockyDoor.java
+++ src/main/java/com/lothrazar/blockydoors/BlockyDoor.java
@@ -1,10 +1,11 @@
 package com.lothrazar.blockydoors;
 
 import net.minecraft.world.level.block.DoorBlock;
+import net.minecraft.world.level.block.state.properties.BlockSetType;
 
 public class BlockyDoor extends DoorBlock {
 
-  public BlockyDoor(Properties p, String reg) {
-    super(p);
+  public BlockyDoor(Properties p, BlockSetType type) {
+    super(p, type);
   }
 }
--- src/main/java/com/lothrazar/blockydoors/ModRegistry.java
+++ src/main/java/com/lothrazar/blockydoors/ModRegistry.java
@@ -1,16 +1,15 @@
 package com.lothrazar.blockydoors;
 
-import net.minecraft.world.level.block.state.BlockBehaviour.Properties;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.material.Material;
 import java.util.ArrayList;
 import java.util.List;
-import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour.Properties;
+import net.minecraft.world.level.block.state.properties.BlockSetType;
+import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.RegisterEvent;
@@ -18,13 +17,13 @@ import net.minecraftforge.registries.RegisterEvent;
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModRegistry {
 
-  private static CreativeModeTab TAB = new CreativeModeTab(ModMain.MODID) {
-
-    @Override
-    public ItemStack makeIcon() {
-      return new ItemStack(DOOR_STONE);
-    }
-  };
+  //  private static CreativeModeTab TAB = new CreativeModeTab(ModMain.MODID) {
+  //
+  //    @Override
+  //    public ItemStack makeIcon() {
+  //      return new ItemStack(DOOR_STONE);
+  //    }
+  //  };
   public static BlockyDoor DOOR_STONE;
   public static BlockyDoor DOOR_COBBLESTONE;
   public static BlockyDoor DOOR_MOSSY_COBBLESTONE;
@@ -56,37 +55,37 @@ public class ModRegistry {
 
   @SubscribeEvent
   public static void onBlocksRegistry(RegisterEvent event) {
-    event.register(Registry.BLOCK_REGISTRY, r -> {
-      r.register("door_stone", createDoor("", Blocks.STONE, Properties.of(Material.STONE)));
-      r.register("door_smooth_stone", createDoor("", Blocks.STONE, Properties.of(Material.STONE)));
-      r.register("door_cobblestone", createDoor("", Blocks.COBBLESTONE, Properties.of(Material.STONE)));
-      r.register("door_mossy_cobblestone", createDoor("", Blocks.MOSSY_COBBLESTONE, Properties.of(Material.STONE)));
-      r.register("door_stone_bricks", createDoor("", Blocks.STONE_BRICKS, Properties.of(Material.STONE)));
-      r.register("door_blackstone", createDoor("", Blocks.BLACKSTONE, Properties.of(Material.STONE)));
-      r.register("door_polished_blackstone_bricks", createDoor("", Blocks.POLISHED_BLACKSTONE_BRICKS, Properties.of(Material.STONE)));
-      r.register("door_granite", createDoor("", Blocks.GRANITE, Properties.of(Material.STONE)));
-      r.register("door_andesite", createDoor("", Blocks.ANDESITE, Properties.of(Material.STONE)));
-      r.register("door_diorite", createDoor("", Blocks.DIORITE, Properties.of(Material.STONE)));
-      r.register("door_bricks", createDoor("", Blocks.BRICKS, Properties.of(Material.STONE)));
-      r.register("door_prismarine", createDoor("", Blocks.PRISMARINE, Properties.of(Material.STONE)));
-      r.register("door_prismarine_bricks", createDoor("", Blocks.PRISMARINE_BRICKS, Properties.of(Material.STONE)));
-      r.register("door_dark_prismarine", createDoor("", Blocks.DARK_PRISMARINE, Properties.of(Material.STONE)));
-      r.register("door_netherrack", createDoor("", Blocks.NETHERRACK, Properties.of(Material.STONE)));
-      r.register("door_nether_bricks", createDoor("", Blocks.NETHER_BRICKS, Properties.of(Material.STONE)));
-      r.register("door_red_nether_bricks", createDoor("", Blocks.RED_NETHER_BRICKS, Properties.of(Material.STONE)));
-      r.register("door_purpur", createDoor("", Blocks.PURPUR_BLOCK, Properties.of(Material.STONE)));
-      r.register("door_sandstone", createDoor("", Blocks.SANDSTONE, Properties.of(Material.STONE)));
-      r.register("door_red_sandstone", createDoor("", Blocks.RED_SANDSTONE, Properties.of(Material.STONE)));
-      r.register("door_quartz", createDoor("", Blocks.QUARTZ_BLOCK, Properties.of(Material.STONE)));
-      r.register("door_red_mushroom", createDoor("", Blocks.RED_MUSHROOM_BLOCK, Properties.of(Material.STONE)));
-      r.register("door_brown_mushroom", createDoor("", Blocks.BROWN_MUSHROOM_BLOCK, Properties.of(Material.STONE)));
-      r.register("door_end_stone", createDoor("", Blocks.END_STONE, Properties.of(Material.STONE)));
-      r.register("door_end_stone_bricks", createDoor("", Blocks.END_STONE_BRICKS, Properties.of(Material.STONE)));
-      r.register("door_obsidian", createDoor("", Blocks.OBSIDIAN, Properties.of(Material.STONE)));
-      r.register("door_mossy_stone_bricks", createDoor("", Blocks.MOSSY_STONE_BRICKS, Properties.of(Material.STONE)));
-      r.register("door_cracked_stone_bricks", createDoor("", Blocks.CRACKED_STONE_BRICKS, Properties.of(Material.STONE)));
+    event.register(Registries.BLOCK, r -> {
+      r.register("door_stone", createDoor(BlockSetType.STONE, Blocks.STONE, Properties.of(Material.STONE)));
+      r.register("door_smooth_stone", createDoor(BlockSetType.ACACIA, Blocks.STONE, Properties.of(Material.STONE)));
+      r.register("door_cobblestone", createDoor(BlockSetType.ACACIA, Blocks.COBBLESTONE, Properties.of(Material.STONE)));
+      r.register("door_mossy_cobblestone", createDoor(BlockSetType.ACACIA, Blocks.MOSSY_COBBLESTONE, Properties.of(Material.STONE)));
+      r.register("door_stone_bricks", createDoor(BlockSetType.ACACIA, Blocks.STONE_BRICKS, Properties.of(Material.STONE)));
+      r.register("door_blackstone", createDoor(BlockSetType.ACACIA, Blocks.BLACKSTONE, Properties.of(Material.STONE)));
+      r.register("door_polished_blackstone_bricks", createDoor(BlockSetType.ACACIA, Blocks.POLISHED_BLACKSTONE_BRICKS, Properties.of(Material.STONE)));
+      r.register("door_granite", createDoor(BlockSetType.ACACIA, Blocks.GRANITE, Properties.of(Material.STONE)));
+      r.register("door_andesite", createDoor(BlockSetType.ACACIA, Blocks.ANDESITE, Properties.of(Material.STONE)));
+      r.register("door_diorite", createDoor(BlockSetType.ACACIA, Blocks.DIORITE, Properties.of(Material.STONE)));
+      r.register("door_bricks", createDoor(BlockSetType.ACACIA, Blocks.BRICKS, Properties.of(Material.STONE)));
+      r.register("door_prismarine", createDoor(BlockSetType.ACACIA, Blocks.PRISMARINE, Properties.of(Material.STONE)));
+      r.register("door_prismarine_bricks", createDoor(BlockSetType.ACACIA, Blocks.PRISMARINE_BRICKS, Properties.of(Material.STONE)));
+      r.register("door_dark_prismarine", createDoor(BlockSetType.ACACIA, Blocks.DARK_PRISMARINE, Properties.of(Material.STONE)));
+      r.register("door_netherrack", createDoor(BlockSetType.ACACIA, Blocks.NETHERRACK, Properties.of(Material.STONE)));
+      r.register("door_nether_bricks", createDoor(BlockSetType.ACACIA, Blocks.NETHER_BRICKS, Properties.of(Material.STONE)));
+      r.register("door_red_nether_bricks", createDoor(BlockSetType.ACACIA, Blocks.RED_NETHER_BRICKS, Properties.of(Material.STONE)));
+      r.register("door_purpur", createDoor(BlockSetType.ACACIA, Blocks.PURPUR_BLOCK, Properties.of(Material.STONE)));
+      r.register("door_sandstone", createDoor(BlockSetType.ACACIA, Blocks.SANDSTONE, Properties.of(Material.STONE)));
+      r.register("door_red_sandstone", createDoor(BlockSetType.ACACIA, Blocks.RED_SANDSTONE, Properties.of(Material.STONE)));
+      r.register("door_quartz", createDoor(BlockSetType.ACACIA, Blocks.QUARTZ_BLOCK, Properties.of(Material.STONE)));
+      r.register("door_red_mushroom", createDoor(BlockSetType.ACACIA, Blocks.RED_MUSHROOM_BLOCK, Properties.of(Material.STONE)));
+      r.register("door_brown_mushroom", createDoor(BlockSetType.ACACIA, Blocks.BROWN_MUSHROOM_BLOCK, Properties.of(Material.STONE)));
+      r.register("door_end_stone", createDoor(BlockSetType.ACACIA, Blocks.END_STONE, Properties.of(Material.STONE)));
+      r.register("door_end_stone_bricks", createDoor(BlockSetType.ACACIA, Blocks.END_STONE_BRICKS, Properties.of(Material.STONE)));
+      r.register("door_obsidian", createDoor(BlockSetType.ACACIA, Blocks.OBSIDIAN, Properties.of(Material.STONE)));
+      r.register("door_mossy_stone_bricks", createDoor(BlockSetType.ACACIA, Blocks.MOSSY_STONE_BRICKS, Properties.of(Material.STONE)));
+      r.register("door_cracked_stone_bricks", createDoor(BlockSetType.ACACIA, Blocks.CRACKED_STONE_BRICKS, Properties.of(Material.STONE)));
     });
-    event.register(Registry.ITEM_REGISTRY, r -> {
+    event.register(Registries.ITEM, r -> {
       for (BlockyDoor b : BLOCKLIST) {
         String id = b.getDescriptionId().replace(HAX, "");
         r.register(id, createItem(b));
@@ -97,13 +96,13 @@ public class ModRegistry {
   private static final String HAX = "block." + ModMain.MODID + ".";
 
   private static Item createItem(BlockyDoor door) {
-    return new BlockItem(door, new Item.Properties().tab(TAB));
+    return new BlockItem(door, new Item.Properties());
   }
 
   private static final List<BlockyDoor> BLOCKLIST = new ArrayList<>();
 
-  public static BlockyDoor createDoor(String name, Block block, Block.Properties p) {
-    BlockyDoor b = new BlockyDoor(wrap(p, block), name);
+  public static BlockyDoor createDoor(BlockSetType type, Block block, Block.Properties p) {
+    BlockyDoor b = new BlockyDoor(wrap(p, block), type);
     BLOCKLIST.add(b);
     return b;
   }
